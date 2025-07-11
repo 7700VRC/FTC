392e838704140855cb5463a33b210ac4146cce5b
George Kirkman <gkirkman@rollingrobots.com>
2025-07-10 21:09:59 -0700

    first drive code

---
+++ Drive.java
@@ -0,0 +1,116 @@
+/*   MIT License
+ *   Copyright (c) [2024] [Base 10 Assets, LLC]
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+/*
+ * This OpMode is an example driver-controlled (TeleOp) mode for the goBILDA 2024-2025 FTC
+ * Into The Deep Starter Robot
+ * The code is structured as a LinearOpMode
+ *
+ * This robot has a two-motor differential-steered (sometimes called tank or skid steer) drivetrain.
+ * With a left and right drive motor.
+ * 
+ *
+ * The drive wheels are 96mm diameter traction (Rhino) or omni wheels.
+ * They are driven by 2x 5203-2402-0019 312RPM Yellow Jacket Planetary Gearmotors.
+ */
+
+
+@TeleOp(name="FTC Starter Kit Example Robot (INTO THE DEEP)", group="Robot")
+//@Disabled
+public class ConceptGoBildaStarterKitRobotTeleop_IntoTheDeep extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    public DcMotor  leftDrive   = null; //the left drivetrain motor
+    public DcMotor  rightDrive  = null; //the right drivetrain motor
+
+
+    @Override
+    public void runOpMode() {
+        /*
+        These variables are private to the OpMode, and are used to control the drivetrain.
+         */
+        double left;
+        double right;
+
+
+
+        /* Define and Initialize Motors */
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_front_drive"); //the left drivetrain motor
+        rightDrive = hardwareMap.get(DcMotor.class, "right_front_drive"); //the right drivetrain motor
+       
+
+        /* Most skid-steer/differential drive robots require reversing one motor to drive forward.
+        for this robot, we reverse the right motor.*/
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+
+        /* Setting zeroPowerBehavior to BRAKE enables a "brake mode". This causes the motor to slow down
+        much faster when it is coasting. This creates a much more controllable drivetrain. As the robot
+        stops much quicker. */
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+       
+       
+        /* Send telemetry message to signify robot waiting */
+        telemetry.addLine("Robot Ready.");
+        telemetry.update();
+
+        /* Wait for the game driver to press play */
+        waitForStart();
+
+        /* Run until the driver presses stop */
+        while (opModeIsActive()) {
+
+            /* Set the drive and turn variables to follow the joysticks on the gamepad.
+            the joysticks decrease as you push them up. So reverse the Y axis. */
+
+
+            left  = -gamepad1.left_stick_y;
+            right = -gamepad1.right_stick_y;
+
+            //call the drive funtion to make the robot move:
+            driveTank(left,right,10);  // move for 10 msec before checking for a new speed input
+
+
+           
+        }
+        public void driveTank(double leftSpeed, double rightSpeed, double wt) {
+        leftDrive.setPower(leftSpeed);
+        rightDrive.setPower(rightSpeed);
+        sleep(wt);
+         }
+
+       
+    }
+}
